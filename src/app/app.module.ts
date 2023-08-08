426a3eecdef367ce336d40378d8614f5779e6458
apexloaded <admin@apexloaded.com>
2023-08-08 01:43:36 +0100

    PWA added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -14,6 +14,7 @@ import { ReferralService, TransactionService, WalletService } from './services';
 import { AuthGuard } from './guards/auth.guard';
 
 import { AppVersion } from '@awesome-cordova-plugins/app-version/ngx';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -23,6 +24,12 @@ import { AppVersion } from '@awesome-cordova-plugins/app-version/ngx';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     AppVersion,
